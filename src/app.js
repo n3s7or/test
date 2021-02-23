24024498a3e3ff285396f64cdbbcfd9b8ff968da
n3s7or <nrmorales.93@gmail.com>
2021-02-23 18:42:14 -0500

    Removed un-needed layout component

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 import React from "react";
-import Layout from "./components/layout";
-
 import "./scss/global.scss";
+
+import Navbar from "./components/navbar/navbar";
+import Publish from "./components/publish/publish";
+import Comment from "./components/comment/comment";
+import {IconContext} from "react-icons";
+
+import data from "./data.json";
 
 class App extends React.Component{
     render(){
         return(
-            <Layout>
-                {/*<h1>Hello World</h1>*/}
-            </Layout>
-    );
+            <IconContext.Provider value={{ size: "1.5em" }}>
+                <div className="main">
+                    <Navbar />
+                    <Publish />
+                    {data.map((comment, index)=>(
+                        <Comment
+                            key={index}
+                            data={comment}
+                        />
+                    ))}
+                </div>
+            </IconContext.Provider>
+        );
     }
 }
 
