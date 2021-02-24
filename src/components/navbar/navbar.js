74a89b7b720075919c5e2f058d939a4cf977d569
n3s7or <nrmorales.93@gmail.com>
2021-02-24 16:36:52 -0500

    Added simple dropdown

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import { BiMenu } from "react-icons/bi";
 import "./navbar.scss";
 
-export default function Navbar() {
+export default function Navbar({user}) {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <nav className="navbar">
             <ul>
                 <li></li>
                 <li>Domicilios Test</li>
-                <li>
-                    <BiMenu />
+                <li className="dropdown">
+                    <a href="#" onClick={(e)=>{
+                        e.preventDefault();
+                        setShowMenu(!showMenu);
+                    }}><BiMenu /></a>
+                    <div className={showMenu ? "dropdown-content dropdown-show" : "dropdown-content"}>
+                        <p>Hello {user}!</p>
+                        <a href="#" className="button">Publicar</a>
+                    </div>
                 </li>
             </ul>
         </nav>
